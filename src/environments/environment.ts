7f3b008c7ec03b34d0bc313502aa0d89f908a681
wikky2012 <wicliferono@gmail.com>
2020-08-30 02:46:35 +0300

    add styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-  
+  production: false,
+  apiKey:'b0c95ddd552a889048079a59b67e8984eb855522',
 };
 
 /*
