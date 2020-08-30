07192d31c1cbc010285a95403c8088fca667f68d
wikky2012 <wicliferono@gmail.com>
2020-08-30 13:37:43 +0300

    include routing

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,13 +3,14 @@ import { ProfileService } from '../../services/profile.service'
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls:['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
 profile:any;
 repos:any;
 username: string;
+
   constructor(private profileService: ProfileService) {
   
    }
